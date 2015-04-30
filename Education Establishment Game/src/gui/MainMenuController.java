e4a137f672f3b671f378feb399439cc71b7aa912
40041181 <40041181@addd2482-0c65-ce4c-8462-d9fc5e6e9b31>
2015-04-30 00:18:22 +0000

    DM - Added More Screens

---
+++ Education Establishment Game/src/gui/MainMenuController.java
@@ -0,0 +1,62 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * 
+ * JAVAFX -> Code for #events from MainMenu.fxml
+ * 
+ * @author Dean
+ *
+ * @see https://www.youtube.com/watch?v=wOUmUcVbO2s (Totorial on JavaFX)
+ */
+public class MainMenuController implements Initializable{
+	
+	@FXML Button btnPlayGame,btnRules,btnExit;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+	
+	
+	public void playGame(ActionEvent e){
+		try {
+			Stage stage = new Stage();
+			stage.setTitle("Education Establishment Game");	
+		    Pane myPane = null;
+		       
+		    myPane = FXMLLoader.load(getClass().getResource("MainBoard.fxml"));
+			
+		    Scene scene = new Scene(myPane);
+		    stage.setScene(scene);
+
+		    stage.show();
+		    
+		    ((Stage) btnPlayGame.getScene().getWindow()).close();
+		    
+		    
+		      
+		} catch (IOException io) {
+			// TODO Auto-generated catch block
+			io.printStackTrace();
+		}
+	}
+	
+	public void exitGame(ActionEvent e){
+		System.exit(0);
+	}
+	
+
+}
