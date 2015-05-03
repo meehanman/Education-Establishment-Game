1654e01bbc604b4bbaffdbc09226c4c948a08216
40041181 <40041181@addd2482-0c65-ce4c-8462-d9fc5e6e9b31>
2015-05-03 19:09:50 +0000

    DM: Added isDoubles

--- Education Establishment Game/src/utils/Dice.java
+++ Education Establishment Game/src/utils/Dice.java
@@ -39,42 +39,13 @@ public class Dice {
 		values = new int[] {roll(),roll()};
 		return values;
 	}
-	
-	public int[] diceRoll(int diceNum){
-		int[] temp = new int[diceNum];
-		for (int i = 0; i<diceNum; i++){
-			temp[i] = roll();
-		}
-		return temp;
+	/**
+	 * Returns if the values are areDoubles()
+	 * @return
+	 */
+	public boolean isDoubles() {
+		return values[0]==values[1];
 	}
-
-	public boolean checkDoublesRoll(int[]diceRoll) {
-		// check if roll was a double
-		if(diceRoll.length == 0){
-			//if no dice in the array
-			return false;
-		} else {
-			//if all dice don't have the same value. 
-			int firstDiceValue = diceRoll[0];
-			for(int i : diceRoll){
-				if (i != firstDiceValue){
-					return false;
-				}
-			}
-			//if all dice have the same value.
-			return true;
-		}
-	}
-
-	public int add(int[] diceRoll) {
-		// add all values to get movement
-		int diceValue = 0;
-		for(int i : diceRoll){
-			diceValue += i;
-		}
-		return diceValue;
-	}
-	
 	
 
 }
