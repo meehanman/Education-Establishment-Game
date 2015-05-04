769a0b9557086469a07cef9933f908e9d8483383
Dean Meehan <meehanman@users.noreply.github.com>
2015-05-04 00:44:18 +0000

    DM - Just an update, added lots more UI

--- Education Establishment Game/src/board/establishment/Restaurant.java
+++ Education Establishment Game/src/board/establishment/Restaurant.java
@@ -46,7 +46,7 @@ public class Restaurant extends Establishment {
 		if(hasOwner()){
 			for(Square square : squares){
 				//If the square is a Restaurant and the owner is this owner
-				if(square instanceof Restaurant && ((Establishment)(square)).getOwner().equals(getOwner())){
+				if(square.getSquareType().equals("Restaurant") && ((Establishment)(square)).getOwner().equals(getOwner())){
 					amountOwned++;
 				}
 			}
