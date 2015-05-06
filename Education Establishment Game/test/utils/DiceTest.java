bc13a348ab29c7a87b2629e908c40a4dc9f31b3e
40107940 <40107940@addd2482-0c65-ce4c-8462-d9fc5e6e9b31>
2015-05-06 21:08:20 +0000

    testing and mocking structures in
    -Conor Lee

--- Education Establishment Game/test/utils/DiceTest.java
+++ Education Establishment Game/test/utils/DiceTest.java
@@ -2,13 +2,40 @@ package utils;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
+import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DiceTest {
+	
+	Dice dice;
+	int[] values;
+	Random rand;
+	
+	@Before
+	public void setUp(){
+		values = EasyMock.createMock(int[].class);
+		rand = EasyMock.createMock(Random.class);
+		dice = new Dice();
+	}
+	
+	@After
+	public void tearDown(){
+		values = null;
+		rand = null;
+		dice = null;
+	}
 
 	@Test
 	public void testGetValues() {
-		fail("Not yet implemented");
+		values = new int[] {1,2};
+		EasyMock.expect(dice.getValues()).andReturn(EasyMock.aryEq(values));
+		EasyMock.replay(dice);
+		
+		assertArrayEquals(new int[]{1,2}, dice.getValues());
 	}
 
 	@Test
