29fd25aa78dea9a4c970c5b19edd7c6ff62f7b11
40107940 <40107940@addd2482-0c65-ce4c-8462-d9fc5e6e9b31>
2015-05-06 19:47:15 +0000

    basic testing architecture
    -Conor Lee

---
+++ Education Establishment Game/test/board/BoardTest.java
@@ -0,0 +1,16 @@
+package board;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardTest {
+	
+	
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Education Establishment Game/test/utils/DiceTest.java
@@ -0,0 +1,29 @@
+package utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiceTest {
+
+	@Test
+	public void testGetValues() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRollDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsDoubles() {
+		fail("Not yet implemented");
+	}
+
+}
