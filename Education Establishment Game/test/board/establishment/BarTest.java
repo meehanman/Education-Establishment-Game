c383e2b54a80cd2bd5add20eb0da7aec82e67df2
40107940 <conorlee@users.noreply.github.com>
2015-05-06 19:47:15 +0000

    basic testing architecture
    -Conor Lee

---
+++ Education Establishment Game/test/board/establishment/BarTest.java
@@ -0,0 +1,14 @@
+package board.establishment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BarTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
